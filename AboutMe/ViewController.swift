8579634671c9596042dc8ed843cca450c5e58fef
Austin Dobberfuhl <AUSTIN.DOBBERFUHL4597@STU.MTEC.EDU>
2023-09-15 07:09:30 -0600

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Austin Dobberfuhl on 9/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
